a038e593b95e73d3a0174684f6b9a393c8fc65b3
Chris Burr <christopher.burr@cern.ch>
2026-02-18 21:24:16 +0100

    fix: use name|lower in pin_subpackage, disable broken build-time tests
    - pin_subpackage(name) resolved to HepMC3 not hepmc3
    - Disable tests/examples/docs at build time to avoid CMake 4 errors
      (exec_program banned by CMP0153, test source resolution broken)
    - Tests still run via the recipe tests section

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,12 +5,11 @@ cmake ${CMAKE_ARGS} \
     -DCMAKE_BUILD_TYPE=Release \
     -DHEPMC3_ENABLE_ROOTIO=OFF \
     -DHEPMC3_ENABLE_PYTHON=ON \
-    -DHEPMC3_BUILD_DOCS=ON \
-    -DHEPMC3_BUILD_EXAMPLES=ON \
-    -DHEPMC3_ENABLE_TEST=ON \
+    -DHEPMC3_BUILD_DOCS=OFF \
+    -DHEPMC3_BUILD_EXAMPLES=OFF \
+    -DHEPMC3_ENABLE_TEST=OFF \
     -S source \
     -B build
 
 cmake --build build --parallel "${CPU_COUNT}"
 cmake --install build
-ctest --test-dir build
